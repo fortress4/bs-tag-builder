951a9abd31e75afdc4baf57b1f47170046fe2bd9
fortress4 <gac@fortress4.com>
2023-03-06 07:33:48 -0500

    Updates for the typeahead examples

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Dependencies:
 3. [Font-Awesome 6.x](https://fontawesome.com/) (6.3.0)
 4. [HTML5 Sortable v0.13.x](https://lukasoppermann.github.io/html5sortable/) (0.13.3)
 5. [BootboxJS 6.x](http://bootboxjs.com/) (6.0.0)
+6. [jQuery Typeahead Search](http://www.runningcoder.org/jquerytypeahead/)** (2.11.0)
+
+** The jQuery Typeaahead Search library is only required if using the Typeahead option
 
 View the [Demo](https://fortress4.github.io/bs-tag-builder/) page.
 
@@ -15,11 +18,10 @@ Warning: This is a work in progress.
 
 Options:
 
-
 * Autocomplete/TypeAhead
   * Requires:
     * Additional HTML around the form-control input
-    * Autocomplete js library
+    * jQuery Typeaahead Search js library
     * Additional JS/Ajax data call (see tag-builder-typeahead.js)
 
 
--- js/tag-builder-typeahead.js
+++ js/tag-builder-typeahead.js
@@ -41,37 +41,10 @@ $(function() {
                console.log('onSubmit TypeAhead: formSubmitted: ', formSubmitted);
 
                //alert(JSON.stringify(items));
-
-               /!*if ( event.keyCode == 13 ) {
-                  console.log('keyCode Press: Submit');
-
-                  var tb_field = $('#props_aliasListTA');
-                  var tb_bin = $('#props_aliasListTA_bin');
-                  var tb_add_field = $('#props_aliasListTA_add');
-                  tb_addTag(tb_field,tb_bin,tb_add_field,event);
-               }*!/
-
             }*/
          }
          ,debug: false
       });
-
-      /*$('#exampleThree_add').on('keydown.typeahead',function(event){
-
-         console.log('keyDown Triggered: exampleThree_add');
-         //console.log('event: ', event);
-
-         if ( event.keyCode == 13 ) {
-            //console.log('on(keydown.typeahead): keyCode Press 13');
-
-            var tb_field = $('#exampleThree');
-            var tb_bin = $('#exampleThree_bin');
-            var tb_msg = $('#exampleThree_msg');
-            var tb_add_field = $('#exampleThree_add');
-            //tb_addTag(tb_field,tb_bin,tb_msg,tb_add_field,event);
-         }
-
-      });*/
 
       $('#exampleFour_add').typeahead({
          minLength: 1,
@@ -83,29 +56,13 @@ $(function() {
          source: {
          alias: {
                ajax: {
-                   type: "POST",
-                  url: 'data/libs.json',
+                  type: "POST",
+                  url: 'data/libs.html', // Using .html to work around github pages issue
                }
             }
          }
          ,debug: false
       });
-
-      /*$('#exampleFour_add').on('keydown.typeahead',function(event){
-
-         console.log('keyDown Triggered: exampleFour_add');
-         //console.log('event: ', event);
-         if ( event.keyCode == 13 ) {
-            //console.log('on(keydown.typeahead): keyCode Press 13');
-
-            var tb_field = $('#exampleFour');
-            var tb_bin = $('#exampleFour_bin');
-            var tb_msg = $('#exampleFour_msg');
-            var tb_add_field = $('#exampleFour_add');
-
-            //tb_addTag(tb_field,tb_bin,tb_msg,tb_add_field,event);
-         }
-      });*/
 
    }
 
